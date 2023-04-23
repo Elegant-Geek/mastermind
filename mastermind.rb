4b63415b05fec8a621a7e60755098c37b4f5c868
Jamie Clark <clarkcjamie@gmail.com>
2023-04-23 16:32:56 -0400

    add basic round loop construct

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,12 @@
 #3:39 - 3:51pm create random code generator
-# 3:51-4:15 add basic custom set number of guesses!
+# 3:51 - 4:15pm add basic custom set number of guesses!
+# 4:22 - 
 
 #after each guess add one to the counter
 class Game
   attr_reader :board, :players
   # create a list of possible colors
-  COLORS = ["R", "Y", "G", "B", "P"]
+  COLORS = ["Red", "Yellow", "Green", "Blue", "Purple"]
 
   def initialize(game_name = "Mastermind")
     @players = []
@@ -13,6 +14,7 @@ class Game
     # (It's curerntly hidden bc no reader/writer is given for it!)
     @code = []
     # reset()
+    @guess_array = []
     puts "New game called '#{game_name}' created."
   end
 
@@ -42,20 +44,42 @@ class Game
     p @code
   end
 
-  def game_turn()
+  def game_turns()
     # the guesses is hard coded to be 12 
     # (i could do a preferences option using gets.chomp.to_i to grab a number of guesses from the user (set a range like +/- 4 guesses: 8-16 guesses!))
     # then pass that variable in here instead of 12. The ^^ user pref variable is stored in setup btw.
     @guess_amount.times do |n|
-      # testing the guess amount! this will loop the exact amount of n times the user specifies ad then will exit the loop once n times have been reached.
-      puts "Round #{n + 1}:"
+        # testing the guess amount! this will loop the exact amount of n times the user specifies ad then will exit the loop once n times have been reached.
+        puts "Round #{n + 1}:"
+        puts "Current guesses go in this big printed array: #{@guess_array}"
+        4.times do |n|
+          loop do
+            puts "Enter slot #{n + 1} color guess:"
+            @ans = gets.chomp.capitalize
+            if !(COLORS.include?(@ans))
+              puts "Invalid guess: Pick one of these colors: #{COLORS}."
+            elsif (COLORS.include?(@ans))
+                  # once valid entry,
+            puts "worksssssss"
+            break
+        else
+          puts "something is wrong."
+        end
+      end
+      # ^ loop end
+    end
+    # ^ guess amount times |n| loop end
+
+
+
+
     end
   end
 
   def play_game()
     create_code()
     number_of_guesses_setup()
-    game_turn()
+    game_turns()
   end
 
 
